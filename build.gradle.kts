fa94787f72b538ce8e0f1c9549b4025476ee7eb7
Lolita <lolita.11.07.06@gmail.com>
2025-12-22 16:20:51 +0700

    init

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("commons-cli:commons-cli:1.5.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.shadowJar {
+    manifest {
+        attributes("Main-Class" to "app.Main")
+    }
+    archiveBaseName.set("util")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
